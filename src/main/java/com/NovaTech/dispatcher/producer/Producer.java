72f3bbc102c67cead8792ed87ed2e8cbc26b2a04
Bassell-Iddisah <bbasssell16@gmail.com>
2025-06-19 04:36:10 +0000

    refactor: Include basic producer implementation from sampleCode

--- src/main/java/com/NovaTech/dispatcher/producer/Producer.java
+++ src/main/java/com/NovaTech/dispatcher/producer/Producer.java
@@ -1,19 +1,32 @@
-package com.NovaTech.producer;
+package com.NovaTech.dispatcher.producer;
 
-import com.NovaTech.model.Task;
-import lombok.AllArgsConstructor;
-
-import java.util.concurrent.BlockingQueue;
+import com.NovaTech.dispatcher.model.Task;
+import com.NovaTech.dispatcher.repository.SharedTaskQueue;
+import lombok.*;
 
 @AllArgsConstructor
-public class Producer implements Runnable {
-    private int taskNo;
-    private BlockingQueue<Task> taskQueue;
-
+class Producer implements Runnable{
     @Override
     public void run() {
-        for( int i = 0; i < this.taskNo; i++ ) {
-
+        for(int k=1; k<5; k++) {
+            for (int i = 1; i < SharedTaskQueue.getSize(); i++) {
+                //log here
+                System.out.println(Thread.currentThread().getName() + ": creating task " + i);
+                Task task = Task.builder()
+                        .build();
+                try {
+                    SharedTaskQueue.add(task);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            }
+            //delay thread
+            try {
+                Thread.sleep(500);
+                System.out.println();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
-}
